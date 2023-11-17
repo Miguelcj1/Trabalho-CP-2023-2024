33fb3ce492f5302569e709ea3add3553c884f132
Katilho <pemicama12@gmail.com>
2023-11-17 15:14:10 +0000

    core_test.sh without begin date

--- code/scripts/cores_test.sh
+++ code/scripts/cores_test.sh
@@ -4,12 +4,13 @@
 CORES_LIST="4 8 12 16 20 24 28 32 36 40"
 
 # Specify the start time in the format YYYY-MM-DDTHH:MM:SS
-START_TIME="2023-11-16T19:39:00"
+# START_TIME="2023-11-16T19:39:00"
 
 # Iterate through each value in the list
 for CORES in $CORES_LIST; do
     JOB_NAME="job_${CORES}_cores"
     echo "Running SBATCH with $CORES CORES with name $JOB_NAME"
-    # sbatch --output=core_outputs/$JOB_NAME.out --partition=cpar --cpus-per-task=$CORES perf.sh
-    sbatch --output=core_outputs/$JOB_NAME.out --partition=cpar --cpus-per-task=$CORES --begin=$START_TIME scripts/perf.sh
+    sbatch --partition=cpar --cpus-per-task=$CORES --output=core_outputs/$JOB_NAME.out scripts/perf.sh
+    # sbatch --output=core_outputs/$JOB_NAME.out --partition=cpar --cpus-per-task=$CORES --begin=$START_TIME scripts/perf.sh
+    sleep 10
 done
