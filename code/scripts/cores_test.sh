f91ccd877f4bc18d2475894c99fa14dfbd861717
Katilho <pemicama12@gmail.com>
2023-11-23 18:49:52 +0000

    Fixed usage of threads

--- code/scripts/cores_test.sh
+++ code/scripts/cores_test.sh
@@ -2,6 +2,7 @@
 
 # List of CORES values to iterate through
 CORES_LIST="4 8 12 16 20 24 28 32 36 40"
+# CORES_LIST="2 4 8 12 16 20 24 28 32 36 40"
 
 # Specify the start time in the format YYYY-MM-DDTHH:MM:SS
 # START_TIME="2023-11-16T19:39:00"
@@ -10,7 +11,8 @@ CORES_LIST="4 8 12 16 20 24 28 32 36 40"
 for CORES in $CORES_LIST; do
     JOB_NAME="job_${CORES}_cores"
     echo "Running SBATCH with $CORES CORES with name $JOB_NAME"
-    sbatch --partition=cpar --cpus-per-task=$CORES --output=core_outputs/$JOB_NAME.out scripts/perf.sh
+    export OMP_NUM_THREADS=$CORES
+    sbatch --partition=cpar --cpus-per-task=40 --output=core_outputs/$JOB_NAME.out scripts/perf.sh
     # sbatch --output=core_outputs/$JOB_NAME.out --partition=cpar --cpus-per-task=$CORES --begin=$START_TIME scripts/perf.sh
     sleep 5
 done
